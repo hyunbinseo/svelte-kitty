5851832899f14efedf16b07765d524af0a978322
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2025-01-09 15:29:56 +0900

    chore: don't initialize production db

--- packages/create-svelte-kitty/index.js
+++ packages/create-svelte-kitty/index.js
@@ -138,7 +138,6 @@ await p.tasks([
 
 			await execAsync('node --run db:generate');
 			await execAsync('node --run db:migrate');
-			await execAsync('node --run db:migrate:prod');
 
 			const gitIsAvailable = !spawnSync('git', ['--version']).error;
 			if (gitIsAvailable) {
--- packages/create-svelte-kitty/template/src/lib/server/db/index.ts
+++ packages/create-svelte-kitty/template/src/lib/server/db/index.ts
@@ -9,7 +9,7 @@ import * as schema from './schema.ts';
 // Reference https://vitejs.dev/guide/env-and-mode#modes
 
 export const db = drizzle({
-	connection: { source: DATABASE_URL, fileMustExist: true },
+	connection: { source: DATABASE_URL },
 	casing: 'snake_case',
 	schema
 });
