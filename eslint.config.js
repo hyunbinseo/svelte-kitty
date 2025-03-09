5e9ec9f3055aefc937d4798ab9f1cb9af9ecdbf9
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2025-03-09 22:44:50 +0900

    chore: add eslint packages in workspace

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
+import { includeIgnoreFile } from '@eslint/compat';
 import eslint from '@eslint/js';
 import prettier from 'eslint-config-prettier';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
+	includeIgnoreFile(import.meta.dirname + '/.gitignore'),
 	eslint.configs.recommended,
 	...tseslint.configs.recommended,
 	prettier,
@@ -17,8 +19,11 @@ export default tseslint.config(
 	},
 	{
 		rules: {
+			'@typescript-eslint/no-restricted-imports': ['error', 'assert', 'node:assert'],
+			'@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
 			'no-console': ['error', { allow: ['warn', 'error'] }],
-			'no-restricted-imports': ['error', 'assert', 'node:assert'],
+			'no-restricted-imports': 'off',
+			'no-unused-vars': 'off',
 		},
 	},
 );
