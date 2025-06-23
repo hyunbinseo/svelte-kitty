5fb6215bdb24b04cb081b2198c2aaa912c0d106e
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-06-24 03:43:49 +0900

    refactor: place favicon in static

--- packages/create-svelte-kitty/template/src/routes/+layout.svelte
+++ packages/create-svelte-kitty/template/src/routes/+layout.svelte
@@ -1,12 +1,7 @@
 <script>
-	import { dev } from '$app/environment';
 	import { page } from '$app/state';
 	import { PUBLIC_SITE_NAME } from '$env/static/public';
-	import logo from '$lib/static/logo.svg?no-inline';
 	import '../app.css';
-
-	// SVG favicons are not supported by Safari 18.
-	// Reference https://caniuse.com/link-icon-svg
 
 	let { children } = $props();
 </script>
@@ -25,10 +20,6 @@
 	</title>
 	<meta property="og:title" content={page.data.pageTitle || PUBLIC_SITE_NAME} />
 	<meta property="og:site_name" content={PUBLIC_SITE_NAME} />
-	<!-- Blocked by https://github.com/sveltejs/svelte/issues/15337 -->
-	{#if !dev}
-		<link rel="icon" href={logo} />
-	{/if}
 	<script>
 		if (window.location.pathname !== '/unsupported') {
 			var isSupported =
