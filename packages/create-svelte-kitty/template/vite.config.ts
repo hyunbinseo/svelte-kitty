b770df2c475ffcd6a01b63da601ac01a8f619398
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-09-21 14:11:00 +0900

    chore: specify css target with nesting

--- packages/create-svelte-kitty/template/vite.config.ts
+++ packages/create-svelte-kitty/template/vite.config.ts
@@ -2,6 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	build: { target: 'es2023' },
+	// Reference https://caniuse.com/sr_es14
+	// Reference https://caniuse.com/css-nesting
+	build: { target: 'es2023', cssTarget: ['firefox117'] },
 	plugins: [sveltekit()],
 });
