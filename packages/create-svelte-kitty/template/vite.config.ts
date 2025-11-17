49302bcdfc6ccad91f1f4de50733c9b1b003f9d9
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-11-17 17:49:25 +0900

    chore: delete copied static content

--- packages/create-svelte-kitty/template/vite.config.ts
+++ packages/create-svelte-kitty/template/vite.config.ts
@@ -1,7 +1,28 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { globSync, rmSync } from 'node:fs';
+import { join } from 'node:path';
+import { env } from 'node:process';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	build: { target: 'es2023' },
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		{
+			name: 'delete-static',
+			closeBundle: () => {
+				const { BUILD_ID } = env;
+				if (!BUILD_ID) return;
+
+				const outDir = join(import.meta.dirname, './build', BUILD_ID);
+
+				const paths = globSync('./client/*', {
+					exclude: ['./client/_app'],
+					cwd: outDir,
+				}).map((path) => join(outDir, path));
+
+				for (const path of paths) rmSync(path, { recursive: true });
+			},
+		},
+	],
 });
