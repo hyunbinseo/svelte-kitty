cfbf63e32297d8b262a619c58e0e76cb3adc796d
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2026-05-19 08:20:36 +0900

    chore: remove @eslint/compat

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,18 @@
-import { includeIgnoreFile } from '@eslint/compat';
-import eslint from '@eslint/js';
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import { defineConfig, includeIgnoreFile } from 'eslint/config';
 import globals from 'globals';
 import { resolve } from 'node:path';
-import tseslint from 'typescript-eslint';
+import ts from 'typescript-eslint';
 
-export default tseslint.config(
+export default defineConfig(
 	includeIgnoreFile(resolve(import.meta.dirname, '.gitignore')),
-	eslint.configs.recommended,
-	tseslint.configs.recommended,
+	js.configs.recommended,
+	ts.configs.recommended,
+	prettier,
 	{
-		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node,
-			},
-		},
+		languageOptions: { globals: { ...globals.browser, ...globals.node } },
+		rules: { 'no-undef': 'off' },
 	},
 	{
 		rules: {
